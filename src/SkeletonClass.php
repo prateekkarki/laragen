c16b6f55ebe3c348c431ba0c5460096b2899d097
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-12-24 12:03:39 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/SkeletonClass.php
+++ src/SkeletonClass.php
@@ -4,23 +4,23 @@ namespace Prateekkarki\Laragen;
 
 class SkeletonClass
 {
-	/**
-	 * Create a new Skeleton Instance
-	 */
-	public function __construct()
-	{
-		// constructor body
-	}
+    /**
+     * Create a new Skeleton Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
 
-	/**
-	 * Friendly welcome
-	 *
-	 * @param string $phrase Phrase to return
-	 *
-	 * @return string Returns the phrase passed in
-	 */
-	public function echoPhrase($phrase)
-	{
-		return $phrase;
-	}
+    /**
+     * Friendly welcome
+     *
+     * @param string $phrase Phrase to return
+     *
+     * @return string Returns the phrase passed in
+     */
+    public function echoPhrase($phrase)
+    {
+        return $phrase;
+    }
 }
