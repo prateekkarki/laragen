58c0ce494542636f9c33db96d71bf801cb239dff
vikalstha <lakiv.esor@gmail.com>
2019-02-25 19:10:42 +0545

    Change file structure

--- src/Commands/Generate.php
+++ src/Commands/Generate.php
@@ -26,10 +26,8 @@ class Generate extends Command
         'css' => 'public',
         'js' => 'public',
         'fonts' => 'public',
-        'stubs/Views/layouts/backend/app.stub' => 'resources/views/backend/layouts/app.blade.php',
-        'stubs/Views/layouts/backend/login.stub' => 'resources/views/backend/auth/login.blade.php',
-        'stubs/Controllers/backend/LoginController.stub'     => 'app/Http/Controllers/Backend/Auth/LoginController.php',
-        'stubs/Controllers/backend/DashboardController.stub' => 'app/Http/Controllers/Backend/DashboardController.php'
+        'resources/views/backend' => 'resources/views',
+        'resources/controllers'   => 'app/Http/Controllers',
     ];
     /**
      * Execute the console command.
