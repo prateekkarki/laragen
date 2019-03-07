60bf0656d252487d5d2b30c1e02ef17c9e75e93d
Prateek Karki <pratiek.karki@gmail.com>
2019-03-07 14:43:07 +0545

    Update config structure

---
+++ config/modules.php
@@ -0,0 +1,43 @@
+<?php
+
+// Data options separated by '|'
+// Arguments for options separated by ':'
+
+// Regular data types: 
+//      string, int, text, bool, date
+
+// Data type options 
+//      unique, required
+
+//Must start with data type nd then followed by size, then by modifiers if required
+
+// Special data types: 
+//      parent: requires name of a module, creates a one-to-many relation with the current module
+//      related: requires name of a module, creates many to many relation with current module
+
+return [
+    'categories' => [
+        'title'             => 'string:128',
+        'slug'              => 'string:128|unique',
+        'short_description' => 'string',
+        'show_in_menu'      => 'boolean',
+        'image'             => 'image',
+        'banner'            => 'image|mimes:jpeg,png,gif',
+        'brochure'          => 'file|mimes:pdf|max:500000'
+    ],
+
+    'tags' => [
+        'title'             => 'string:128'
+    ],
+
+    'posts' => [
+        'author'            => 'parent:users',
+        'title'             => 'string:128',
+        'slug'              => 'string:128|unique',
+        'short_description' => 'string:512',
+        'full_description'  => 'text',
+        'category'          => 'parent:categories',
+        'posted_at'         => 'datetime',
+        'tags'              => 'related:tags'
+    ],
+];
---
+++ config/options.php
@@ -0,0 +1,22 @@
+<?php
+return [
+	'items_to_generate' => [
+		'Common' => [
+			'Migration',
+			'Model',
+			'Seeder',
+			'Route'
+		],
+		'Frontend' => [
+			'Controller',
+			'View'
+		],
+		'Backend' => [
+			'Controller',
+			'Request',
+			'View'
+		]
+	],
+	'skip_generators' => [],
+	'override' => true
+];
