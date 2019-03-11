d9c10f1f2cf36474c3f4840a09cc282bcd777784
Prateek Karki <pratiek.karki@gmail.com>
2019-03-11 23:32:24 +0545

    Add some UI components

--- src/resources/views/backend/layouts/main.blade.php
+++ src/resources/views/backend/layouts/main.blade.php
@@ -7,6 +7,12 @@
         @include('backend.includes.sidebar')
 
         <main class="main">
+            
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+            </ol>
+
             <div class="container-fluid">
                 <div class="animated fadeIn">
                     @yield('content')
