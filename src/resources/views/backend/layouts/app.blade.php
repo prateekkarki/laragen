04c030c1ab82af703c2216dc0c879bd76b997b0a
Prateek Karki <pratiek.karki@gmail.com>
2019-03-19 17:10:11 +0545

    Add dropzone back

--- src/resources/views/backend/layouts/app.blade.php
+++ src/resources/views/backend/layouts/app.blade.php
@@ -37,6 +37,7 @@
             <script src="{{ asset('js/jquery-3.3.1.min.js') }}" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
             <script src="{{ asset('js/popper.min.js') }}" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
             <script src="{{ asset('js/bootstrap.min.js') }}" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+            <script src="{{ asset('js/dropzone.min.js') }}"></script>
             <script src="{{ asset('js/jquery.nicescroll.min.js') }}"></script>
             <script src="{{ asset('js/moment.min.js') }}"></script>
             <script src="{{ asset('js/stisla.js') }}"></script>
