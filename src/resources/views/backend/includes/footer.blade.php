63ef38d418df989d30d27e3599b75f4ec62292f7
Prateek Karki <pratiek.karki@gmail.com>
2019-04-16 15:53:15 +0545

    Add version number on footer

--- src/resources/views/backend/includes/footer.blade.php
+++ src/resources/views/backend/includes/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; {{ date('Y') }} <div class="bullet"></div> 
+        Copyright &copy; Laragen {{ date('Y') }} <div class="bullet"></div> 
         <!-- Developed By <a href="#"> MagicSquare</a> -->
     </div>
     <div class="footer-right">
-        0.0.2
+        0.0.4
     </div>
 </footer>
