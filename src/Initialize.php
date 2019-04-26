d67417394b6ce6d450bd5f5d4f428b885584f4a3
vikalstha <lakiv.esor@gmail.com>
2019-04-26 13:00:56 +0545

    cmpsr test 6

--- src/Initialize.php
+++ src/Initialize.php
@@ -10,6 +10,7 @@ class Initialize
     {
         echo "init...........";
         $composer = $event->getComposer();
+        var_dump($composer);
         copy(__DIR__ . '/../src/resources/stubs/RouteServiceProvider.stub', __DIR__ . '/../../../../app/Providers/LaragenRouteServiceProvider.php');
         if (!is_dir('routes/backend')) {
             @mkdir(__DIR__ . '/../../../../routes/backend', 0777, true);
@@ -20,6 +21,22 @@ class Initialize
             @mkdir(__DIR__ . '/../../../../routes/frontend', 0777, true);
             copy(__DIR__ . '/../src/resources/stubs/Route.stub', __DIR__ . '/../../../../routes/frontend/web.php');
         }
+    }
+
+    public static function preInstall(Event $event)
+    {
+        echo "pre install funcsn";
+
+    }
+
+    public static function postInstall(Event $event)
+    {
+        $composer = $event->getComposer();
+    }
+
+    public static function postPackageInstall(Event $event)
+    {
+        $installedPackage = $event->getComposer()->getPackage();
     }
 
 }
