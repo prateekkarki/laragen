6861a269d571c8582336540e1865c2f54399a756
Prateek Karki <pratiek.karki@gmail.com>
2019-05-21 23:15:42 +0545

    Use js for greeting

--- src/resources/resources/views/backend/auth/layout.blade.php
+++ src/resources/resources/views/backend/auth/layout.blade.php
@@ -25,20 +25,25 @@
         <div class="absolute-bottom-left index-2">
             <div class="text-light p-5 pb-2">
                 <div class="mb-5 pb-3">
-                    <h1 class="mb-2 display-4 font-weight-bold">@php $time = date('H');@endphp
-                        @if ($time < "12")
-                            Good morning
-                        @elseif ($time >= "12" && $time < "17")
-                            Good afternoon
-                        @elseif ($time >= "17" && $time < "19")
-                            Good evening
-                        @else
-                            Good night
-                        @endif
-                    </h1>
+                    <h1 class="mb-2 display-4 font-weight-bold" id="login-greeting"></h1>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    var d = new Date();
+    var h = d.getHours();
+    var greeting = '';
+    if (h < 12) 
+        greeting = 'Good morning';
+    else if(h < 17)
+        greeting = 'Good afternoon';
+    else if(h < 19)
+        greeting = 'Good evening';
+    else
+        greeting = 'Good night';
+    document.getElementById("login-greeting").innerHTML = greeting;
+</script>
 @endsection
