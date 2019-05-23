8a09c74a19d172c1280ac7cdba0aa5dbbb63ecb0
Prateek Karki <pratiek.karki@gmail.com>
2019-05-23 23:07:05 +0545

    Add image size to options

--- config/options.php
+++ config/options.php
@@ -18,6 +18,11 @@ return [
 			'View'
 		]
 	],
+	'image_sizes' => [
+		'sm' => '500x500',
+		'md' => '800x800',
+		'xs' => '200x200',
+	],
 	'seed_rows' => 25,
 	'listing_per_page' => 20,
 	'user_model' => 'App\User'
