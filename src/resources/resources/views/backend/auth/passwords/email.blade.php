3b6fab6723e1b6175e678152a39fe563d9ee5213
Prateek Karki <pratiek.karki@gmail.com>
2019-05-23 22:19:21 +0545

    Add links

--- src/resources/resources/views/backend/auth/passwords/email.blade.php
+++ src/resources/resources/views/backend/auth/passwords/email.blade.php
@@ -13,23 +13,21 @@
     <form method="POST" action="{{ route('backend.password.email') }}">
         @csrf
 
-        <div class="form-group row">
-            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control @if($errors->has('email')) is-invalid @endif" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                @if ($errors->has('email'))
-                    <span class="invalid-feedback">{{ $errors->first('email') }}</span>
-                @endif
-            </div>
+        <div class="form-group">
+            <label for="email">Email Address</label>
+            <input id="email" type="email" class="form-control @if ($errors->has('email')) is-invalid @endif" name="email" tabindex="1" required autocomplete="email" autofocus>
+            @if ($errors->has('email'))
+                <span class="invalid-feedback">{{ $errors->first('email') }}</span>
+            @endif
         </div>
 
-        <div class="form-group row mb-0">
-            <div class="col-md-6 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Send Password Reset Link') }}
-                </button>
-            </div>
+        <div class="form-group text-right">
+            <a href="{{ route('backend.login') }}" class="float-left mt-3">
+                Go back
+            </a>
+            <button type="submit" class="btn btn-primary btn-lg btn-icon icon-right" tabindex="4">
+                {{ __('Reset Password') }}
+            </button>
         </div>
     </form>
 @endsection
--- src/resources/resources/views/backend/includes/sidebar.blade.php
+++ src/resources/resources/views/backend/includes/sidebar.blade.php
@@ -2,19 +2,13 @@
 
 <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-        <a href="/">{{ config('app.name', 'Laragen Dashboard') }}</a>
+        <a href="{{ route('backend.dashboard') }}">{{ config('app.name', 'Laragen Dashboard') }}</a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
-        <a href="/">Lg</a>
+        <a href="{{ route('backend.dashboard') }}">Lg</a>
     </div>
     
     @include('backend.includes.main_menu')
-   
-    <!-- <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-        <a href="docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-            <i class="fas fa-rocket"></i> Documentation
-        </a>
-    </div> -->
 </aside>
     
 </div>
